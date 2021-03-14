fbc49e530a452dce4f3cb648d3383952113e010c
kenpulicorre <ingkeneidel@gmail.com>
2021-03-14 12:38:59 -0500

    se adiciono directorio para creacion libreria statica

---
+++ staticlib/oper.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int suma(int a,int b) {
+	printf("voy a sumar\n");fflush(stdout);
+	return a + b;
+}
+int resta(int a,int b) {
+ return a - b;
+}
+
---
+++ staticlib/usooper.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+#include "oper.h"
+
+int main(int argc, char *argv[]){
+	int a = 5, b = 6;
+	return suma(a, resta(b, 7));
+
+}
+
